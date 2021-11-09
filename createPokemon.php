75d76dcac9e6404b6d0798d656663f7e0c8007dc
DeoDurant <choieanson@hotmail.com>
2021-11-08 21:43:07 -0600

    Created and implemented a create pokemon page

---
+++ createPokemon.php
@@ -0,0 +1,56 @@
+<?php
+//Creates a new blog post.
+require 'authenticate.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Professor Oak's Pokedex - New Pokemon</title>
+
+</head>
+<body>
+    <h1>Professor Oak's Pokedex - New Pokemon</h1>
+    <section>
+        <section>
+            <h1><a href="index.php">Home</a></h1>
+        </section>
+
+        <ul>
+            <li><a href="index.php" class="active">Pokedex</a></li>
+            <li><a href="createPokemon.php">New Post</a></li>
+        </ul>
+
+        <section>
+            <form action="process.php" method="post">
+                <fieldset>
+                <p>
+                    <label for="name">Name of the Pokemon: </label>
+                    <input name="name" id="name">
+                </p>
+                <p>
+                    <label for="typing">Projected typing: </label>
+                    <input name="typing" id="typing"></input>
+                </p>
+                <p>
+                    <label for="ability">Projected ability: </label>
+                    <input name="ability" id="ability"></input>
+                </p>
+                <p>
+                    <label for="notes">Notes: </label>
+                    <textarea name="notes" id="notes"></textarea>
+                </p>
+                <p>
+                    <input type="submit" name="command" value="Create">
+                </p>
+                </fieldset>
+            </form>
+        </section>
+        
+        <section>
+            <?php date('d,m,y'); ?>
+        </section>
+    </section>
+</body>
+</html>
