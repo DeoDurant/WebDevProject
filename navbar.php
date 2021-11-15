44104cb917e88d1a771f264a1e7c4ea9ec53f1ad
DeoDurant <choieanson@hotmail.com>
2021-11-15 17:06:00 -0600

    Implemented a navbar.

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<div class="header">
+        <div class="menu-bar">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">Professor Oak's Pokedex</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="discussion.php">Discussion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="createPokemon.php">New Pokemon</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="createPost.php">New Post</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
